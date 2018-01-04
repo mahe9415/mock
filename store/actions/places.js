442d6b2381556b13382599ca6cee048e7e6aef0b
mahe94 <mahesh9415@gmail.com>
2018-01-04 18:55:47 +0530

    redux setup

---
+++ store/actions/places.js
@@ -0,0 +1,32 @@
+import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from './actionType.js'
+
+
+export const addPlace = placeName => {
+	return {
+		type: ADD_PLACE,
+		placeName:placeName
+	}
+}
+
+export const deletePlace = () => {
+	return{
+		type:DELETE_PLACE
+	}
+}
+
+
+export const selectPlace = key => {
+	return{
+		type:SELECT_PLACE,
+		placeKey:key
+	}
+}
+
+
+export const deselect = () =>{
+	return{
+		type:DESELECT_PLACE
+	}
+}
+
+
---
+++ store/reducers/places.js
@@ -0,0 +1,46 @@
+import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from './actions/actionType'
+	
+
+
+	const initialState = {
+		places=['Chennai'],
+		selectedPlace:null
+	}
+
+	const reducer = (state = initialState , action) =>{
+		switch (action.type) {
+			case ADD_PLACE :
+			return {
+				...state,
+				places:state.places.concat({
+					name:action.placeName,
+					key:Math.random(),
+					image:{
+						url:"https://developers.giphy.com/static/img/sample_emoji.002b24a680c8.png"
+					}
+				})
+			}
+			case DELETE_PLACE : 
+			return {
+				...state,
+				places : state.places.filter( item => item.key != state.selectedPlace.key ),
+				selectedPlace : null
+			}
+			case SELECT_PLACE :
+			return {
+				...state,
+				selectedPlace : state.places.find( item => item.key === action.placeKey)
+			}
+
+			case DESELECT_PLACE :
+			return {
+				..state,
+				selectedPlace:null
+			}
+			default:
+				return state;
+		}
+	};
+
+
+	export default reducer;
